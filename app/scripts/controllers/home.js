b6c1a02b0017d32452ab5d0ca4db06dc2f6ab1a0
Hector E. Gomez Morales <hectoregm@gmail.com>
2014-05-17 21:58:39 -0500

    Added primitive modal for adding task.

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -1,6 +1,23 @@
 'use strict';
 
-app.controller('HomeCtrl', function($scope, $ionicNavBarDelegate, $state) {
+app.controller('HomeCtrl', function($scope,
+                             $ionicNavBarDelegate,
+                             $state,
+                             $ionicModal) {
   console.log('In Home');
   $scope.state = $state;
+
+  $ionicModal.fromTemplateUrl('templates/new-task.html', function(modal) {
+    $scope.taskModal = modal;
+  }, {
+    scope: $scope
+  });
+
+  $scope.newTask = function() {
+    $scope.taskModal.show();
+  };
+
+  $scope.closeNewTask = function() {
+    $scope.taskModal.hide();
+  };
 });
